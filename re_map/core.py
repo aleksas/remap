12adcb9d1ce774b9f5c5046e1bf1e14395639d0c
Aleksas Pielikis <ant.kampo@gmail.com>
2019-09-29 23:16:45 +0300

    Add span intersection validation.

--- re_map/core.py
+++ re_map/core.py
@@ -15,35 +15,16 @@ def len_delta(match_start, replacement_span_map):
 
     return delta
 
-def validate(entry_1, i, replacement_span_map):
-    if len(replacement_span_map) <= i or i < 0:
-        return
-
-    entry_2 = replacement_span_map[i]
-
-    # uses assum[ption that span[0] < span[1]
-    ops = [
-        lambda x, y : (x[0] < y[0]) == (x[1] < y[0]),
-        lambda x, y : (x[0] > y[0]) == (x[0] > y[1]),
-        lambda x, y : (x[0] == y[0]) == (x[1] == y[1])
-    ]
-
-    def cmp(span1, span2):
-        valid = True
-        for op in ops:
-            valid = valid and op(span1, span2)
-        return valid
-
-    valid = cmp(entry_1[0], entry_2[0])
-    valid = valid and cmp(entry_1[1], entry_2[1])
-
-    #if not valid:
-    #    raise Exception("New entry intersects with other entry")
-
 def sub(span, value):
     return span[0] - value, span[1] - value
 
 def insert(entry, current_span, replacement_span_map):
+    def validate(source_span, ref_source_span):
+        if ref_source_span[0] <= source_span[0] and ref_source_span[1] > source_span[0]:
+            raise Exception("Illegal san intersection")
+        elif ref_source_span[0] <= source_span[1] and ref_source_span[1] > source_span[1]:
+            raise Exception("Illegal span intersection") 
+
     delta = span_len_delta(entry[1], entry[0])
 
     i=0
@@ -51,19 +32,25 @@ def insert(entry, current_span, replacement_span_map):
     for i in range(len(replacement_span_map) + 1): # +1 for i to actually reach last element
         if i == len(replacement_span_map):
             break
-        if replacement_span_map[i][0][0] > entry[0][1]:
+
+        source_span = replacement_span_map[i][0]
+        
+        if source_span[0] > entry[0][1]:
             break
-        if replacement_span_map[i][0] == entry[0]:
+        if source_span == entry[0]:
             replace = True
             break
 
     if replace:
-        entry = replacement_span_map[i][0], entry[1]
         del replacement_span_map[i]
 
-    validate(entry, i - 1, replacement_span_map)
+    if i > 0:
+        validate(entry[0], replacement_span_map[i-1][0])
+
     replacement_span_map.insert(i, entry)
-    validate(entry, i + 1, replacement_span_map)
+
+    if i < len(replacement_span_map) - 1:
+        validate(entry[0], replacement_span_map[i+1][0])
 
     for j in range(i+1, len(replacement_span_map)):
         replacement_span_map[j] = replacement_span_map[j][0], (replacement_span_map[j][1][0] + delta, replacement_span_map[j][1][1] + delta)
