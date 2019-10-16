80fc6a1cf8b49ffefbf0e7785c11e9c9baa8b908
Aleksas Pielikis <ant.kampo@gmail.com>
2019-10-16 11:50:36 +0300

    Cean test

--- tests/test_single_modifier.py
+++ tests/test_single_modifier.py
@@ -44,33 +44,5 @@ class SingleModifierTestCase(TestCase):
         self.assertEqual( decorated_text, '011233' )
         self.assertEqual( decorated_processed_text, '001223' )
 
-    def test_new(self):
-        numbers = {5: 'five', 8: 'eight', 10: 'ten'}
-        orginal_numbers = {1: 'first', 2: 'second'}
-
-        modifiers = [
-            ( r'der (G\.) Be',  { 1: 'Graham'} ),
-            ( r' (&) ',  { 1: 'and'} ),
-            ( r' (etc)\.',  { 1: 'et cetera'} ),
-            ( r' ((\d+)((st)|(nd)|(rd)|(th))) ',  { 2: lambda x: orginal_numbers[int(x)], 3: '' } ),
-            ( r' (\d+) ',  { 1: lambda x: numbers[int(x)] } ),
-            ( r'([^ ]+)',  { 1: lambda x: x } ),
-        ]
-
-        text = 'Alexander G. Bell ate 10 apples & 8 cucumbers. The 1st apple was rotten, the 2nd was too, also the third, fourth etc.'
-
-        processed_text, span_map = process(text, modifiers)
-
-        decorated_text, decorated_processed_text = utils.decorate(text, processed_text, span_map)
-
-        print (text)
-        print (decorated_text)
-        print (processed_text)
-        print (decorated_processed_text)
-        print (span_map)
-
 if __name__ == '__main__':
-    #tc = SingleModifierTestCase()
-    #tc.test_new()
-
     main()
